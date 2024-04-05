7a9dab1a1b31c8750900c4fc57b51e904d5ee896
olayinka <olayinkaolodude5@gmail.com>
2024-04-05 01:22:36 +0100

    javascript update

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const newNav = document.querySelector(".newnav")
 const banner = document.querySelector(".monito");
 const hero = document.querySelector("#heroleft");
 const cancel = document.querySelector(".close");
+const searchIcon = document.querySelector("#inputimg");
+const searchArea = document.querySelector(" #hright input");
 
 
 
@@ -14,4 +16,9 @@ menu.addEventListener("click", function(){
 cancel.addEventListener("click", function(){
   newNav.style.display = "none"
   menu.style.display = "block"
+})
+
+searchIcon.addEventListener("click", function(){
+  searchArea.style.display = "block"
+  // searchIcon.style.display = "none"
 })
